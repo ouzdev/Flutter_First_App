8ca91ba2ab15918862fd7e69fe5bdadb6a228688
Oğuzcan Genç <mail@oguzcangenc.com>
2021-03-31 22:08:07 +0300

    Eklenen paketteki WordPair sınıfının static random metodu ile bir metin oluşturup final tipte word değişkenine aktarıldı. Ve Text widgetları içinde tanımlanarak ekrana çizdirildi.

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final word = WordPair.random();
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: Text(word.asCamelCase),
         ),
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+          child: Container(child: Text(word.asPascalCase)),
         ),
       ),
     );
