91583c6be1fed71e034100b7b63af49d662d0790
Oğuzcan Genç <mail@oguzcangenc.com>
2021-03-31 22:18:36 +0300

    Stateful bir widget oluşturuldu ve random word üreten bu kütüphane bu sınıf içinde tanımlanarak main.dart içinde kullanıldı.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:flutter_first_app/random_word.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final word = WordPair.random();
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text(word.asCamelCase),
+          title: RandomWords(),
         ),
         body: Center(
-          child: Container(child: Text(word.asPascalCase)),
+          child: Container(child: RandomWords()),
         ),
       ),
     );
